d0fe1f768a1721958f944f04b8594ab63b04dd8f
andrearomeo04 <pisco004@gmail.com>
2024-10-20 19:18:16 +0200

    [UPD] milestone 4 completata

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+import axios from 'axios'
+
+export default {
+     data(){
+      return{
+        projects: []
+      }
+     }, methods:{
+      getProjects(){
+        axios.get('http://127.0.0.1:8000/api/projects').then((res) => {
+          console.log(res.data)
+        })
+      }
+     }, created(){
+      this.getProjects();
+     }
+}
+</script>
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="text-center">I miei progetti</h1>
+        </div>
+        <div class="col-12">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+  @import './styles/generals.scss'
+</style>
