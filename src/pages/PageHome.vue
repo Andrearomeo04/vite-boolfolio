d4d6900164751881900bb884273a3238c85c15b4
andrearomeo04 <pisco004@gmail.com>
2024-10-21 18:46:59 +0200

    [UPD] created router

---
+++ src/pages/PageHome.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 mt-3 mb-5">
+                <h1>Boolfolio</h1>
+            </div>
+            <div class="col-12 mt-3">
+                <p>Ciao sono Andrea Romeo e qui potrai scoprire il mio portfolio personale</p>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+
+</style>
