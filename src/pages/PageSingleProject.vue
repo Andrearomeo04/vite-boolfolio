6716aad69d593c145d04d30a90e110157355f7a3
andrearomeo04 <pisco004@gmail.com>
2024-10-22 21:17:17 +0200

    [UPD] created single project page

---
+++ src/pages/PageSingleProject.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped></style>
