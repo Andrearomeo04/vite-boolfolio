522c6f971377f78b6316594da2084ebab6e939f2
andrearomeo04 <pisco004@gmail.com>
2024-10-22 21:52:48 +0200

    [UPD] added info to show page

--- src/pages/PageSingleProject.vue
+++ src/pages/PageSingleProject.vue
@@ -22,10 +22,18 @@ export default {
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>titolo</h1>
+                <img :src="project.image && project.image.startsWith('http') ? project.image : `http://127.0.0.1:8000/storage/${project.image}`"
+                    alt="">
             </div>
             <div class="col-12">
-
+                <h1>{{ project.title }}</h1>
+                <p v-if="project.technologies.length > 0">Tecnologie:
+                    <span class="pe-2" v-for="tech in project.technologies">
+                        {{ tech.name }}
+                    </span>
+                </p>
+                <p v-else>Nessuna tecnologia usata</p>
+                <p>Tipologia: {{ project.type !== null ? project.type.name : 'nessuna tipologia presente' }}</p>
             </div>
         </div>
     </div>
