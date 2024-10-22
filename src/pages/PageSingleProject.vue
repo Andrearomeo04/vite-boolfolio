c3e3574b282b8dc4d0120dbdd96b6c2b4deda414
andrearomeo04 <pisco004@gmail.com>
2024-10-22 22:06:51 +0200

    [UPD] added created hook

--- src/pages/PageSingleProject.vue
+++ src/pages/PageSingleProject.vue
@@ -9,17 +9,19 @@ export default {
         }
     }, methods: {
         getProject() {
-            axios.get(`${store.baseUrl}/projects/$this.$route.params.slug`).then((res) => {
+            axios.get(`${store.baseUrl}/projects/${this.$route.params.slug}`).then((res) => {
                 if (res.data.success) {
-                    this.project = res.data.result
+                    this.project = res.data.results
                 }
             })
         }
+    }, created() {
+        this.getProject();
     }
 }
 </script>
 <template>
-    <div class="container">
+    <div class="container" v-if="project">
         <div class="row">
             <div class="col-12">
                 <img :src="project.image && project.image.startsWith('http') ? project.image : `http://127.0.0.1:8000/storage/${project.image}`"
@@ -28,7 +30,7 @@ export default {
             <div class="col-12">
                 <h1>{{ project.title }}</h1>
                 <p v-if="project.technologies.length > 0">Tecnologie:
-                    <span class="pe-2" v-for="tech in project.technologies">
+                    <span class="pe-2" v-for="tech in project.technologies" :key="tech.id">
                         {{ tech.name }}
                     </span>
                 </p>
