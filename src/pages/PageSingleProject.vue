d61662344c7501cb9ecff7107fb98b8b9be76ed7
andrearomeo04 <pisco004@gmail.com>
2024-10-22 21:41:35 +0200

    [UPD] added axios request

--- src/pages/PageSingleProject.vue
+++ src/pages/PageSingleProject.vue
@@ -1,15 +1,29 @@
 <script>
+import axios from 'axios'
+import { store } from '../store.js'
 export default {
     data() {
         return {
-
+            store,
+            project: null
         }
-    },
+    }, methods: {
+        getProject() {
+            axios.get(`${store.baseUrl}/projects/$this.$route.params.slug`).then((res) => {
+                if (res.data.success) {
+                    this.project = res.data.result
+                }
+            })
+        }
+    }
 }
 </script>
 <template>
     <div class="container">
         <div class="row">
+            <div class="col-12">
+                <h1>titolo</h1>
+            </div>
             <div class="col-12">
 
             </div>
